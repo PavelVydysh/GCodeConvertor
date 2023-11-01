f68e8d316044e7eebefa4771f4cb9bb43c588cb3
Pavel Vydysh <vydysh.pavel@gmail.com>
2023-11-01 18:54:57 +0300

    Добавьте файлы проекта.

---
+++ GCodeConvertor/MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GCodeConverter
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ICreateType createType;
+        TopologyModel topologyModel;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            setCreateType(new CreateTypeA());
+            topologyModel = new TopologyModel();
+            this.DataContext = topologyModel;
+        }
+
+        private void CreateProjectA(object sender, RoutedEventArgs e)
+        {
+            createType.create(topologyModel, this);
+        }
+
+        void setCreateType(ICreateType createType)
+        {
+            this.createType = createType;
+        }
+    }
+
+    interface ICreateType
+    {
+        void create(TopologyModel topologyModel, MainWindow window);
+    }
+
+    class CreateTypeA : ICreateType
+    {
+        public void create(TopologyModel topologyModel, MainWindow window)
+        {
+            if (String.IsNullOrEmpty(topologyModel.error))
+            {
+                ProjectWindow pw = new ProjectWindow();
+                pw.Show();
+                window.Close();
+            }
+            else
+            {
+                MessageBox.Show("В настройках проекта указаны не все поля, либо допущены ошибки в оформлении полей.",
+                                "[Code 0] Не удалось создать проект",
+                                MessageBoxButton.OK,
+                                MessageBoxImage.Error);
+            }
+        }
+    }
+
+    class CreateTypeB : ICreateType
+    {
+        public void create(TopologyModel topologyModel, MainWindow window)
+        {
+
+        }
+    }
+}
---
+++ GCodeConvertor/ProjectWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Media.Animation;
+
+namespace GCodeConverter
+{
+    /// <summary>
+    /// Логика взаимодействия для ProjectWindow.xaml
+    /// </summary>
+    public partial class ProjectWindow : Window
+    {
+        public ProjectWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void openMenu(object sender, RoutedEventArgs e)
+        {
+
+            DoubleAnimation anim = new DoubleAnimation();
+            if (RightMenu.Width > 0)
+            {
+                anim.From = 250;
+                anim.To = 0;
+                anim.Duration = TimeSpan.FromSeconds(0.2);
+                RightMenu.BeginAnimation(WidthProperty, anim);
+                return;
+            }
+
+            anim.From = 0;
+            anim.To = 250;
+            anim.Duration = TimeSpan.FromSeconds(0.2);
+            RightMenu.BeginAnimation(WidthProperty, anim);
+        }
+
+        private void Canvas_OnMouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.MouseDevice.LeftButton == MouseButtonState.Pressed)
+            {
+                CanvasMain.Children.Add(
+                    new Line
+                    {
+                        X1 = e.GetPosition(CanvasMain).X,
+                        Y1 = e.GetPosition(CanvasMain).Y,
+                        X2 = e.GetPosition(CanvasMain).X + 1,
+                        Y2 = e.GetPosition(CanvasMain).Y + 1,
+                        StrokeStartLineCap = PenLineCap.Round,
+                        StrokeEndLineCap = PenLineCap.Round,
+                        StrokeThickness = 1,
+                        Stroke = Brushes.Black
+                    });
+            }
+        }
+
+        private void Canvas_(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+    }
+}
